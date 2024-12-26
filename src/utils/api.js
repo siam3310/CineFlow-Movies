4c70059dc555a4a4e8a29d883c8e16b62c73e360
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-12-26 09:55:11 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = "614923b097c805c62a593b1827db1524";
 
 const headers = {
     Authorization: `Bearer ${TMDB_TOKEN}`,
